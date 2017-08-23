7fc9540276b6e37a53b8a683bf5ab72b453b1a4b
LoganW94 <loganwogoman@gmail.com>
2017-08-23 19:33:57 -0400

    progress

--- Game/dijkstra.py
+++ Game/dijkstra.py
@@ -21,10 +21,7 @@ class Path:
 		2: path_south,
 		3: path_west}
 
-		#next_tile = self.find_path(start, map)
-
-		"temp code"
-		temp_next_tile = start
+		next_tile = self.find_path(start, map)
 		return(temp_next_tile)
 
 	def	find_path(self, start, map):
@@ -32,6 +29,7 @@ class Path:
 		distance = 0
 
 		while distance < self.max_distance:
+			self.new_tiles = []
 			distance += 1
 			path_num = 0
 			for x in self.find_neighbors(location):
@@ -39,13 +37,17 @@ class Path:
 					for y in self.checked_tiles:
 						if x[0] != y["Location"][0] and x[1] != y["Location"][1]:
 							self.new_tiles.append(self.new_tile(x, map, distance))
+							self.tile_number += 1
 				else:
 					new_tile = self.new_tile(x, map, distance)
 					self.new_tiles.append(new_tile)
 					self.paths[path_num].append(new_tile)
+					self.tile_number += 1
 
 			for x in self.new_tiles:
-				if len(self.checked_tiles) = 0:
+				self.checked_tiles.append(x)
+
+			print(self.checked_tiles)
 
 	def find_neighbors(self, location):
 		x = location[0]
@@ -69,65 +71,13 @@ class Path:
 
 	def determine_value(self, map, location):	
 		value = 0
+		unit = map[location[0]][location[1]][1]
 
-		if isinstance(map[location[0]][location[1]][1], Units)
+		if isinstance(unit, Units)
 			value -= 2
-			if isinstance(map[location[0]][location[1]][1], Player)
+			if isinstance(unit, Player)
 				value -= 2
 
 		return(value)
 
 	
-						
-
-
-
-
-
-
-
-#gist.github.com/econchick/4666413
-
-class Dijsktra():
-	"example that is probably useless"
-	def __init__(self):
-		self.nodes = set()
-		self.edges = defaultdict(list)
-		self.distances = {}
-
-	def add_node(self, value):
-		self.nodes.add(value)
-
-	def add_edge(self, from_node, to_node, distance):
-	self.edges[from_node].append(to_node)
-    self.edges[to_node].append(from_node)
-    self.distances[(from_node, to_node)] = distance
-
-	def dijsktra(graph, initial):
-		visited = {initial: 0}
-		path = {}
-
-		nodes = set(graph.nodes)
-
-		while nodes: 
-			min_node = None
-			for node in nodes:
-				if node in visited:
-			    	if min_node is None:
-			    		min_node = node
-			    	elif visited[node] < visited[min_node]:
-			      		min_node = node
-
-			if min_node is None:
-			  	break
-
-			nodes.remove(min_node)
-			current_weight = visited[min_node]
-
-			for edge in graph.edges[min_node]:
-			  	weight = current_weight + graph.distance[(min_node, edge)]
-			  	if edge not in visited or weight < visited[edge]:
-			    	visited[edge] = weight
-			    	path[edge] = min_node
-
-		return visited, path
