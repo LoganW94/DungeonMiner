58ad6a70fc6b38d2a9a9a370edf021d9dfd26e56
LoganW94 <loganwogoman@gmail.com>
2017-09-02 18:08:32 -0400

    work on pathfinder

--- Game/pathfinder.py
+++ Game/pathfinder.py
@@ -15,6 +15,7 @@ class Path:
 		tile_number = 0
 		checked_tiles = []
 		unchecked_tiles = []
+		target = None
 
 		tiles = self.find_neighbors(start, map, map_size)
 		checked_tiles.append(start_tile)
@@ -36,23 +37,49 @@ class Path:
 					if y not in checked_tiles and y not in unchecked_tiles:
 						self.set_tile_info(y, tile_number, distance, x)
 						tile_number +=1
-						new_tiles.append(y)
-			
+						if y.tile_info()["Is_passable"] == True and :
+							new_tiles.append(y)
+		
+		for t in checked_tiles:
+			self.find_value(t, map)
+
+		"start at the bottom and form all possible paths and return the one with the least total value"
+		"if there is a target find path to target with least value"
+
+		if target == None:
+			"find path of least resistance"
+		else:
+			"find easiest path to target"							
+
 		"return next tile"
 		temp_next_tile = map[start[0]][start[1]][0].tile_info()["Location"]
 		return temp_next_tile
 
 	def find_value(self, tile, map):
+		unit_val = 5
+		grass_val = 2
+		dirt_val = 1
+		obj_val = 5
 		tile_info = tile.tile_info()
 		tile_location = tile_info["Location"]
 		value = 0
+
+		if isinstance(map[tile_location[0]][tile_location[1]][1], Unit):
+			value += unit_val
+		elif isinstance(map[tile_location[0]][tile_location[1]][1], Object):
+			value += obj_val
+		if tile_info["ID"] == "005":
+			value += dirt_val
+		elif tile_info["ID"] == "004"
+			value += grass_val
+
+		value += tile_info["Distance"]	
 
 		tile.set_info("Value", value)
 		
 	def set_tile_info(self, tile, tile_number, distance, predeccesor):
 		tile.set_info("Number", tile_number)
 		tile.set_info("Distance", distance)
-		tile.set_info("Value", distance)
 		tile.set_info("Predeccesor", predeccesor)
 
 	def find_neighbors(self, location, map, map_size):
